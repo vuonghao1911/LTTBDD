4aeaea5974f7a2c8c77e366bbc22026fd7725979
Nguyen Vuong Hao <vuonghao14298@gmail.com>
2022-02-22 09:31:48 +0700

    chuyen thu muc

--- Lap02/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Lap02/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btnH;
-    //hao
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
