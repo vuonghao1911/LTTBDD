1ef47522e6f5b67c9df0872a9786dd4c6e54b7be
Nguyen Vuong Hao <vuonghao14298@gmail.com>
2022-04-24 13:36:52 +0700

    update button

--- Lap08/app/src/main/java/com/example/myapplication/MainActivity2.java
+++ Lap08/app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -1,14 +1,58 @@
 package com.example.myapplication;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    private EditText etPass;
+    private EditText etUser;
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        etPass = findViewById(R.id.idPass);
+        etUser = findViewById(R.id.idUser);
+        btnLogin = findViewById(R.id.idSignIn);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = etUser.getText().toString();
+                String pass = etPass.getText().toString();
+                if (user.equals("")||pass.equals("")){
+                    Toast.makeText(getApplication(),"loi",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(user,pass)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Log.d(TAG,"thanh cong") ;
+                        }
+                        else {
+                            Log.d(TAG, "onComplete: " + task.getException().getLocalizedMessage());
+                        }
+                    }
+                });
+            }
+        });
+
     }
 }
