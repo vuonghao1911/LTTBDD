c81854065d9d19dfe9dd921c7ab988ebd167147b
Nguyen Vuong Hao <vuonghao14298@gmail.com>
2022-04-26 21:35:11 +0700

    test commit

--- Lap08/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Lap08/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,7 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+/*
+hao
+ */
 public class MainActivity extends AppCompatActivity {
     private Button btnSign;
     private  Button btnReg;
