ce20a0237ec7840145c60add50a8c30de2e542cf
Nguyen Vuong Hao <vuonghao14298@gmail.com>
2022-04-26 14:56:12 +0700

    update

--- Lap08/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Lap08/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnSign;
+    private  Button btnReg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnReg =findViewById(R.id.idBtnReg);
+        btnSign = findViewById(R.id.idBtnSignin);
+
+        btnSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,MainActivity2.class);
+                    startActivity(i);
+            }
+        });
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,MainActivity3.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
